aa2885d147d2d5202a25335612db3797e488e91d
Matse007 <matthiastobin@web.de>
2021-08-24 14:42:56 +0200

    Fixed the streamingRole and added some memes

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -6,20 +6,24 @@ module.exports = {
     const guildid = cfg.streamingguilds.booptroop;
     guild = client.guilds.cache.get(guildid);
     //this requires that a streaming role does exist.
-    role = guild.roles.cache.find((r) => r.name === "Streaming");
+    streamrole = guild.roles.cache.find((r) => r.name === "Streaming");
     if (!newPresence.activities) return false;
     let lenghtvar = newPresence.activities.length;
     newPresence.activities.forEach((activity) => {
       if (activity.name === "Twitch" && activity.state === "A Hat in Time") {
         console.log(new Date().toLocaleString());
-        newPresence.member.roles.add(role).catch(console.error);
+        newPresence.member.roles.add(streamrole).catch(console.error);
         console.log(newPresence.member.user.tag + " assigned Stream Role");
       } else {
-        newPresence.member.roles.remove(role).catch(console.error);
+        if(newPresence.member.roles.cache.has(streamrole.id)) {
+        newPresence.member.roles.remove(streamrole).catch(console.error);
+        }
       }
     });
-    if (newPresence.activities.length == 0) {
-      newPresence.member.roles.remove(role).catch(console.error);
+    if (newPresence.activities.length == 0 && newPresence.member.roles.cache.has(streamrole.id)) {
+
+        newPresence.member.roles.remove(streamrole).catch(console.error);
+        
     }
     console.log();
   },
