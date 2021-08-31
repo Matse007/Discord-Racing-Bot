3349bc49b9c04216841a085752c58eecbdfea3a0
Matse007 <matthiastobin@web.de>
2021-09-01 01:01:44 +0200

    Added return to presenceUpdate.

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -6,7 +6,7 @@ module.exports = {
     const guildid = cfg.streamingguilds.booptroop;
     guild = client.guilds.cache.get(guildid);
     //this requires that a streaming role does exist.
-    streamrole = guild.roles.cache.find((r) => r.name === "Streaming");
+    streamrole = guild.roles.cache.find((r) => r.name === "Streaming"); 
     if (!newPresence.activities) return false;
     let lenghtvar = newPresence.activities.length;
     newPresence.activities.forEach((activity) => {
@@ -14,14 +14,14 @@ module.exports = {
         console.log(new Date().toLocaleString());
         newPresence.member.roles.add(streamrole).catch(console.error);
         console.log(newPresence.member.user.tag + " assigned Stream Role");
+        return;
       } else {
-        if(newPresence.member.roles.cache.has(streamrole.id)) {
+        if(newPresence.member.roles.cache.has(streamrole.id)) {   
         newPresence.member.roles.remove(streamrole).catch(console.error);
         }
       }
     });
     if (newPresence.activities.length == 0 && newPresence.member.roles.cache.has(streamrole.id)) {
-
         newPresence.member.roles.remove(streamrole).catch(console.error);
         
     }
